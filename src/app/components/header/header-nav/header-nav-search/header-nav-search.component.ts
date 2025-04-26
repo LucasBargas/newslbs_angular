bee282d8282caa329c5bf4670d8e6ccb50add5f6
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-04-25 21:13:15 -0300

    Social list itens - created

--- src/app/components/header/header-nav/header-nav-search/header-nav-search.component.ts
+++ src/app/components/header/header-nav/header-nav-search/header-nav-search.component.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ErrorMessageComponent } from '../../../error-message/error-message.component';
 import { HeaderComponent } from '../../header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header-nav-search',
   standalone: true,
-  imports: [CommonModule, FormsModule, ErrorMessageComponent],
+  imports: [CommonModule, FormsModule, ErrorMessageComponent, FontAwesomeModule],
   templateUrl: './header-nav-search.component.html',
   styleUrl: './header-nav-search.component.scss'
 })
@@ -16,6 +18,7 @@ export class HeaderNavSearchComponent {
   @Input() mobile!: boolean;
   error: boolean = false;
   value: string = '';
+  faMagnifyingGlass = faMagnifyingGlass
 
   constructor(
     private router: Router,
