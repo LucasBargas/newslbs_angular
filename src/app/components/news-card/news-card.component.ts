e6d8529c67ca31e76632224fef982a1bff3e6ac0
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-04-28 22:52:07 -0300

    News card component - developed

--- src/app/components/news-card/news-card.component.ts
+++ src/app/components/news-card/news-card.component.ts
@@ -3,16 +3,23 @@ import { Component, Input } from '@angular/core';
 import { INews } from '../../interfaces/INews';
 import { RouterLink } from '@angular/router';
 import { NewsService } from '../../services/news.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye  } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare  } from '@fortawesome/free-solid-svg-icons';
+import { faTrash  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-news-card',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FontAwesomeModule],
   templateUrl: './news-card.component.html',
   styleUrl: './news-card.component.scss'
 })
 export class NewsCardComponent {
   @Input() news!: INews;
+  faEye = faEye;
+  faPenToSquare = faPenToSquare;
+  faTrash = faTrash;
 
   constructor(private newsService: NewsService) {}
 
