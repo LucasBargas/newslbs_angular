00d01e9db6b0b7ac29e825ce5886ca26e377e9f0
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-04-30 20:29:45 -0300

    Loading added on news single page

--- src/app/pages/news-single/news-single.component.ts
+++ src/app/pages/news-single/news-single.component.ts
@@ -5,15 +5,17 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { INews } from '../../interfaces/INews';
 import { NewsCardComponent } from "../../components/news-card/news-card.component";
+import { LoadingComponent } from "../../components/loading/loading.component";
 
 @Component({
   selector: 'app-news-single',
   standalone: true,
-  imports: [CommonModule, ContainerComponent, NewsCardComponent],
+  imports: [CommonModule, ContainerComponent, NewsCardComponent, LoadingComponent],
   templateUrl: './news-single.component.html',
   styleUrl: './news-single.component.scss'
 })
 export class NewsSingleComponent implements OnInit {
+  isLoading: boolean = false;
   news!: INews[];
   favorites!: boolean;
   viewNews = false;
@@ -22,6 +24,8 @@ export class NewsSingleComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
+      this.newsService.isLoading$.subscribe(loading => this.isLoading = loading);
+
       const id = params.get('id');
       this.newsService.getNewsById(Number(id)).subscribe((news) => {
         this.news = [news];
