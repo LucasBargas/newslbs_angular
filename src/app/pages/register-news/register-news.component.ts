c7d4b435fee7111c50ae18913d6d913efcbef970
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-02 01:26:03 -0300

    Register page - in development

--- src/app/pages/register-news/register-news.component.ts
+++ src/app/pages/register-news/register-news.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../components/container/container.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register-news',
-  imports: [ContainerComponent],
+  standalone: true,
+  imports: [CommonModule, ContainerComponent],
   templateUrl: './register-news.component.html',
   styleUrl: './register-news.component.scss'
 })
