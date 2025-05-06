1156a8ab91cd68c78aaf4b7ca0ae5ec7447809a7
Lucas Bargas <lucasbargasoficial@gmail.com>
2025-05-05 21:14:19 -0300

    Favorites page - in development

--- src/app/pages/favorites-news/favorites-news.component.ts
+++ src/app/pages/favorites-news/favorites-news.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowcaseComponent } from '../../components/showcase/showcase.component';
+import { ContainerComponent } from "../../components/container/container.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites-news',
   standalone: true,
-  imports: [CommonModule, ShowcaseComponent],
+  imports: [CommonModule, ContainerComponent, RouterLink],
   templateUrl: './favorites-news.component.html',
   styleUrl: './favorites-news.component.scss'
 })
 export class FavoritesNewsComponent {
-  favorites: boolean = true;
-  currentRoute: string = '/noticias-favoritas';
+
 }
