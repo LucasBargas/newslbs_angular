c2fdbb34fbb35b1573e0a9112ee76cd0afc5c2f0
Lucas Bargas <lucas.bargasoficial@gmail.com>
2025-06-30 20:47:05 -0300

    first commit

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,22 +1,22 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BehaviorSubject, finalize, map, Observable } from 'rxjs';
 import { News } from '../models/news.model';
 import { environment } from '../../environments/environment.prod';
 import { SupabaseConfigService } from './supabase-config.service';
+import { CategoriesSignalService } from './categories-signal.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsService {
+  private _categoriesSignal = inject(CategoriesSignalService);
+  category = this._categoriesSignal.category;
+  private _http = inject(HttpClient);
+  private _supabaseConfig = inject(SupabaseConfigService);
   private _API_URL = `${environment.supabaseUrl}/rest/v1/news`;
   public searchLoading$ = new BehaviorSubject<boolean>(true);
   public loading$ = new BehaviorSubject<boolean>(false);
-
-  constructor(
-    private http: HttpClient,
-    private supabaseConfig: SupabaseConfigService,
-  ) {}
 
   or(term: string): string {
     const likeQuery = `*${term}*`;
@@ -28,9 +28,9 @@ export class NewsService {
     const params = new HttpParams().set('or', this.or(term));
     this.searchLoading$.next(true);
 
-    return this.http
+    return this._http
       .get<News[]>(this._API_URL, {
-        headers: this.supabaseConfig.headers(),
+        headers: this._supabaseConfig.headers(),
         params,
       })
       .pipe(
@@ -52,7 +52,12 @@ export class NewsService {
     let params = new HttpParams();
 
     if (search) {
-      params = params.set('or', this.or(search));
+      const orParam =
+        this.category().length > 0
+          ? `(category.ilike.*${search}*)`
+          : this.or(search);
+
+      params = params.set('or', orParam);
     }
 
     if (order && order !== 'aleat') {
@@ -63,10 +68,10 @@ export class NewsService {
       params = params.set('isFavorite', 'eq.true');
     }
 
-    return this.http
+    return this._http
       .get<News[]>(this._API_URL, {
         params,
-        headers: this.supabaseConfig.headers(),
+        headers: this._supabaseConfig.headers(),
       })
       .pipe(
         finalize(() => {
@@ -85,9 +90,9 @@ export class NewsService {
       .set('select', '*')
       .set('limit', 1);
 
-    return this.http
+    return this._http
       .get<News[]>(this._API_URL, {
-        headers: this.supabaseConfig.headers(),
+        headers: this._supabaseConfig.headers(),
         params,
       })
       .pipe(
@@ -101,9 +106,9 @@ export class NewsService {
   }
 
   postNews(news: News): Observable<News> {
-    return this.http
+    return this._http
       .post<News[]>(this._API_URL, news, {
-        headers: this.supabaseConfig.headers(),
+        headers: this._supabaseConfig.headers(),
       })
       .pipe(map((result) => result[0]));
   }
@@ -111,9 +116,9 @@ export class NewsService {
   updateNews(id: string, data: Partial<News>): Observable<News> {
     const url = `${this._API_URL}?id=eq.${id}`;
 
-    return this.http
+    return this._http
       .patch<News[]>(url, data, {
-        headers: this.supabaseConfig.headers(),
+        headers: this._supabaseConfig.headers(),
       })
       .pipe(map((result) => result[0]));
   }
